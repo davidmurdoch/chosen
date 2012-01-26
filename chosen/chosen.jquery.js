d7d29b0eec0eee9b3ecc8d6c6cdc4630c81c8baa
David Murdoch <hello@vervestudios.co>
2012-01-26 17:08:13 -0500

    Add option for default text

--- chosen/chosen.jquery.js
+++ chosen/chosen.jquery.js
@@ -107,7 +107,7 @@ Copyright (c) 2011 by Harvest
       this.options = options != null ? options : {};
       this.set_default_values();
       this.is_multiple = this.form_field.multiple;
-      this.default_text_default = this.is_multiple ? "Select Some Options" : "Select an Option";
+      this.default_text_default = this.options.default_text||(this.is_multiple ? "Select Some Options" : "Select an Option");
       this.setup();
       this.set_up_html();
       this.register_observers();
